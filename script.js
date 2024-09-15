69a4c57ac0084807d6015df33eadffea977ad5aa
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-09-15 12:29:55 +0500

    get elements start

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () =>{
     const productForm = document.getElementById('prductForm');
+    const productList = document.getElementById('productList');
+    const productNameInput = document.getElementById('productName');
+    const productPriceInput = document.getElementById('productPrice');
+    
 })
